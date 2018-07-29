3f26873e15ec87d1db6bc4f0befffbdc4c62b282
flying-pikachu <634588290@qq.com>
2018-07-30 06:07:04 +0800

    添加配置

--- src/main/java/cn/edu/dlnu/masterSpringMVC/MasterSpringMvcApplication.java
+++ src/main/java/cn/edu/dlnu/masterSpringMVC/MasterSpringMvcApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MasterSpringMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MasterSpringMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringMvcApplication.class, args);
+    }
 }

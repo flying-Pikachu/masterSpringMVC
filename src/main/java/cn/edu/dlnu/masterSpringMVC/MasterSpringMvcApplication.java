8e22ea13cfe54827d8280d8522c21a8489075849
flying-pikachu <634588290@qq.com>
2018-07-29 15:43:12 +0800

    完成配置

---
+++ src/main/java/cn/edu/dlnu/masterSpringMVC/MasterSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.dlnu.masterSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterSpringMvcApplication.class, args);
+	}
+}
